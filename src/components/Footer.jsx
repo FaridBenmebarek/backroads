2225ec281c2901d6521e57b786ff21f6cc5627d2
Farid Benmebarek <farid.benmebarek@hotmail.fr>
2020-04-08 18:08:15 +0200

    add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import AniLink from "gatsby-plugin-transition-link/AniLink";
+import AniLink from "gatsby-plugin-transition-link/AniLink"
 import styles from "../css/footer.module.css"
 import links from "../constants/links"
 import socialIcons from "../constants/social-icons"
@@ -9,20 +9,43 @@ const Footer = () => {
     <footer className={styles.footer}>
       <div className={styles.links}>
         {links.map((item, index) => {
-          return (<AniLink fade key={index} to={item.path}>{item.text}</AniLink>)
+          return (
+            <AniLink fade key={index} to={item.path}>
+              {item.text}
+            </AniLink>
+          )
         })}
-        <AniLink cover direction="right" bg="#fff" key={4} to="/contact">contact</AniLink>
+        <AniLink cover direction="right" bg="#fff" key={4} to="/contact">
+          contact
+        </AniLink>
       </div>
       <div className={styles.icons}>
-        {socialIcons.map((item, index)=>{
-          return (<a key={index} href={item.url} target="_blank" rel="noopener noreferrer">{item.icon}</a>)
+        {socialIcons.map((item, index) => {
+          return (
+            <a
+              key={index}
+              href={item.url}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {item.icon}
+            </a>
+          )
         })}
       </div>
       <div className={styles.copyright}>
-        copyright &copy; Backroads Travel Company {new Date().getFullYear()} tous droits réservés
+        copyright &copy; Backroads Travel Company {new Date().getFullYear()}{" "}
+        tous droits réservés
       </div>
       <div className={styles.amiral}>
-        Réalisé par <a href="https://farid-benmebarek.fr" target="_blank" rel="noopener noreferrer">Amiral du Web</a>
+        Réalisé par{" "}
+        <a
+          href="http://farid-benmebarek.fr"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Farid Benmebarek
+        </a>
       </div>
     </footer>
   )
